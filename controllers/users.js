d09b0d2b29dac345146e94a5736b94da6219892b
Anton <antonlaid@yandex.ru>
2023-05-24 21:06:44 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -100,14 +100,13 @@ const updataUser = (req, res, next) => {
       });
     })
     .catch((error) => {
-      // if (error.code === USER_NOT_UNIQUE_ERROR) {
-      //   return next(new ConflictError(MSG_EMAIL_DUPLICATION));
-      // }
-      // if (error.name === VALIDATION_ERROR) {
-      //   return next(new BadRequestError(MSG_INVALID_DATA));
-      // }
-      // return next(error);
-      res.status(502).send("dsf");
+      if (error.code === USER_NOT_UNIQUE_ERROR) {
+        return next(new ConflictError(MSG_EMAIL_DUPLICATION));
+      }
+      if (error.name === VALIDATION_ERROR) {
+        return next(new BadRequestError(MSG_INVALID_DATA));
+      }
+      return next(error);
     });
 };
 
